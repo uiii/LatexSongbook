182d507b15fda455e0bc3f84db1f8ebf93df6334
Uiii <uiii.dev@gmail.com>
2011-11-16 09:26:19 +0100

    SongEditor - open file from command line

--- SongEditor/SongEditor/main.cpp
+++ SongEditor/SongEditor/main.cpp
@@ -1,10 +1,19 @@
 #include <QtGui/QApplication>
 #include "SongEditor.hpp"
 
+#include <QFileInfo>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     SongEditor w;
+
+    if(argc > 1)
+    {
+        w.openSong(argv[1]);
+    }
+
     w.show();
 
     return a.exec();
